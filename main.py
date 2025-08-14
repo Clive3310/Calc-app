22da9ebefa42d258555dde89fbea6039592185ef
Timofej <kolosovtimofej7@gmail.com>
2025-08-14 19:09:25 +0300

    Add more Text_panel methods + keyboard support

--- main.py
+++ main.py
@@ -6,22 +6,32 @@ pg.init()
 class Text_panel:
     
     def __init__(self, text) -> None:
-        self.font = pg.font.SysFont('Arial', 30)
+        self.font = pg.font.SysFont('Calibri', 30)
         self.color = (0, 0, 0)
         self.text = text
         self.ren_text = self.font.render(text, True, self.color)
+
+        self.limit_length = 33
+        self.good_symb = '0123456789-+/*().^'
     
     def update(self, text: str):
         self.text = text
         self.ren_text = self.font.render(text, True, self.color)
     
-    def add(self, text: str):
-        self.text = self.text + text
-        self.ren_text = self.font.render(self.text, True, self.color)
+    def add(self, symb: str):
+        if symb in self.good_symb and len(self.text) < self.limit_length:
+            self.text = self.text + symb
+            self.ren_text = self.font.render(self.text, True, self.color) 
+    
+    def b_space(self):
+        if self.text:
+            self.text = self.text[:-1]
+            self.ren_text = self.font.render(self.text, True, self.color)
     
     def draw(self, surf: pg.Surface, end, height):
         pos = end - self.ren_text.get_width() - 5, height
         surf.blit(self.ren_text, pos)
+
 
 class Output_panel:
 
@@ -42,6 +52,7 @@ class Output_panel:
     def update(self):
         self.s.fill((200, 200, 200))
         self.text_panel.draw(self.s, self.width, self.text_y)
+
 
 class Button_panel:
 
@@ -79,8 +90,11 @@ class Main_window:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     return
-                elif event.type == pg.MOUSEBUTTONDOWN:
-                    self.panel.text_panel.add('1')
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_BACKSPACE:
+                        self.panel.text_panel.b_space()
+                    else:
+                        self.panel.text_panel.add(event.unicode)
             
             self.update()
             screen.blit(self.s, (0, 0))
@@ -95,7 +109,7 @@ class Main_window:
 
 
 def main():
-    text_panel = Text_panel('123')
+    text_panel = Text_panel('')
     panel = Output_panel(400, 100, text_panel)
     button_panel = Button_panel(400, 400)
     window = Main_window(panel, button_panel, 400, 500)
